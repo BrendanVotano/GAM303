e4f86665da340682695e873e129ba7a27ccda9a9
BrendanVotano <brendan@blue-volcano.com>
2018-02-22 09:49:44 +1100

    Initial Commit

---
+++ Assets/Prototype1/Scripts/HighScore.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour {
+
+    static public int score = 1000;
+
+    void Awake()
+    {
+      // If the ApplePickerHighScore already exists, read it
+        if (PlayerPrefs.HasKey("ApplePickerHighScore"))
+        {
+            score = PlayerPrefs.GetInt("ApplePickerHighScore");
+        }
+        // Assign the high score to ApplePickerHighScore
+        PlayerPrefs.SetInt("ApplePickerHighScore", score); // 3
+    }
+
+    void Update()
+    {
+        TextMesh gt = this.GetComponent<TextMesh>();
+        gt.text = "High Score: " + score;
+        // Update ApplePickerHighScore in PlayerPrefs if necessary
+        if (score > PlayerPrefs.GetInt("ApplePickerHighScore"))
+        {
+            PlayerPrefs.SetInt("ApplePickerHighScore", score);
+        }
+    }
+}
