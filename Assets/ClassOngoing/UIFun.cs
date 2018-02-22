b726ca7d12eb5e2010a09a6154448aa680efbb1a
nherrera01 <nicholus.herrera@hotmail.com>
2018-02-22 14:54:40 +1100

    Added Vehicle Package

---
+++ Assets/ClassOngoing/UIFun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFun : MonoBehaviour {
+
+    public Text countdownTimer;
+    public Text playerInput;
+
+    public float timer = 20f;
+
+	// Use this for initialization
+	void Start () {
+        countdownTimer.text = timer.ToString();
+        countdownTimer.color = Color.blue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        countdownTimer.text = timer.ToString("##.#");
+	}
+}
